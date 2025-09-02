6c85ab98962c90bd39dc48625824efb7058db7eb
AlanRik <alanrik2004@gmail.com>
2025-09-02 10:54:22 -0300

    se realizó una mejora

--- src/models/Equipo.ts
+++ src/models/Equipo.ts
@@ -2,26 +2,34 @@ import type { ICompetidor } from "../interface/ICompetidor";
 import { Jugador } from "./Jugador";
 
 export class Equipo implements ICompetidor {
-    nombre:string;
-    private jugadores: Jugador[];
+  nombre: string;
+  private jugadores: Jugador[];
 
-    constructor(nombre:string){
-        this.nombre = nombre;
-        this.jugadores = [];
-    }
-    
-    agregarJugador(jugador: Jugador){
-        this.jugadores.push(jugador);
-    }
+  constructor(nombre: string) {
+    this.nombre = nombre;
+    this.jugadores = [];
+  }
 
-    listarIntegrantes():string[]{
-        return this.jugadores.map(jugador => jugador.toString());
+  agregarJugador(jugador: Jugador) {
+    const existe = this.jugadores.some((j) => j.id === jugador.id);
+    if (existe) {
+      throw new Error(
+        `Ya existe un jugador con el id ${jugador.id} en el equipo`
+      );
     }
+    this.jugadores.push(jugador);
+  }
 
-    toString(): string {
-        return `Equipo: ${this.nombre}, Jugadores: ${this.listarIntegrantes().join(", ")}`;
-    }   
-    get cantidad(): number{
-        return this.jugadores.length;
-    }
+  listarIntegrantes(): string[] {
+    return this.jugadores.map((jugador) => jugador.toString());
+  }
+
+  toString(): string {
+    return `Equipo: ${this.nombre}, Jugadores: ${this.listarIntegrantes().join(
+      ", "
+    )}`;
+  }
+  get cantidad(): number {
+    return this.jugadores.length;
+  }
 }
